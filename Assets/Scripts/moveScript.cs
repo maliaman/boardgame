5aa514a458d789fc2287e88b64cdf2e3aab05256
Matthew Medrano <medranomatthew12@gmail.com>
2020-07-28 18:58:20 -0400

    matts first commit

--- Assets/Scripts/moveScript.cs
+++ Assets/Scripts/moveScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class moveScript : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    // Femboy Hooters
     public Tile currentTile;
     void Start()
     {
